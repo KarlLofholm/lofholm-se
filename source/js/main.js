8209488b75637a329758b4e4c763d4578ceefb5e
Karl Lofholm <karl.lofholm@gmail.com>
2017-11-08 22:14:40 +0100

    Updated to source/build structure

--- source/js/main.js
+++ source/js/main.js
@@ -13,12 +13,10 @@
 
     while (length--) {
         method = methods[length];
- 
+
         // Only stub undefined methods.
         if (!console[method]) {
             console[method] = noop;
         }
     }
-}());  
-
-
+}());
